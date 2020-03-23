1aa42df1c88dbeed83922d43ea7fb72811db52c8
giapnguyen <giapnguyenitf@gmail.com>
2020-03-23 10:46:21 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json([
-        'success' => true,
+        'code' => 200,
+        "message" => 'this is home page'
     ]);
 });
